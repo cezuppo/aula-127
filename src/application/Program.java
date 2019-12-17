31bf23b6db08d617d94d20c7fdf7c85e0afdb890
cezuppo <czuppo@gmail.com>
2019-12-17 07:43:50 -0300

    Product, Imported Product, UsedProduct criados.

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		
+		sc.close();
+	}
+
+}
